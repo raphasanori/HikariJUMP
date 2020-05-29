e55e571e0f7a290f6ccfa5808501ca4de1cd2200
Rapha <61619471+raphasanori@users.noreply.github.com>
2020-05-29 19:49:59 -0300

    Update hikarijump.js

--- hikarijump.js
+++ hikarijump.js
@@ -5,6 +5,12 @@ links[i].href = atob(links[i].href.split("?")[1]);
 }
 }
 
-
-
-
+var all = document.styleSheets;
+for(var i=0;i<all.length;i++){
+if(all[i].href == null){
+var b = all[i].rules[0].cssText;
+if(b.includes("selection")){
+all[i].disabled = true;
+}
+}
+}
